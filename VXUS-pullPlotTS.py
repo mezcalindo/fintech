a27b25a87c2954a2a658545546c05e7949472c32
mezcalindo <9871113+mezcalindo@users.noreply.github.com>
2019-11-05 00:08:36 -0700

    Minor comment editing.

--- VXUS-pullPlotTS.py
+++ VXUS-pullPlotTS.py
@@ -18,7 +18,7 @@ ts = TimeSeries(key, output_format='pandas')
 # vxus_data is a pandas dataframe, vxus_meta_data is a dict
 vxus_data, vxus_meta_data = ts.get_daily(symbol='VXUS')
 
-# Visualizing the data by determining the plot size and style
+# Visualize the data by determining the plot size and style
 figure(num=None, figsize=(15, 6), dpi=80, facecolor='w', edgecolor='k')
 vxus_data['4. close'].plot()
 plt.gca().invert_xaxis()
