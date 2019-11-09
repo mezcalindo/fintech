45c2f911100ad81e94fb1a5c12887036cd203981
mezcalindo <9871113+mezcalindo@users.noreply.github.com>
2019-11-08 22:32:33 -0700

    Coming soon...

--- VXUS-pullViewTS.py
+++ VXUS-pullViewTS.py
@@ -1,18 +1,1 @@
-# Pulling, viewing, and exporting VXUS ETF price data
-
-# Import the software libraries whose functionality we will rely on
-from alpha_vantage.timeseries import TimeSeries
-
-# Input your unique Alpha Vantage API key on the next line
-key = 'INPUT YOUR KEY HERE'
-
-# Choose your output format (pandas tabular), or default to JSON (python dict)
-ts = TimeSeries(key, output_format='pandas')
-
-# Get the data, which returns a tuple of 100 records
-# vxus_data is a pandas dataframe, vxus_meta_data is a dict
-vxus_data, vxus_meta_data = ts.get_daily(symbol='VXUS')
-
-# View the data to verify it's utility and accuracy
-# Print directly in the terminal window
-print(vxus_data.to_string())
+Coming soon...
